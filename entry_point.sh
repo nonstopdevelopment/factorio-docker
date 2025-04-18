eda4b0a55d5f5a4bd2d1311249c3bdecc8917e3a
nonstopdevelopment <bryan+git@nonstopdev.com>
2025-04-17 22:44:18 -0400

    minor cleanup to debugging lines

--- entry_point.sh
+++ entry_point.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 cd /opt/factorio
 
-whoami
-
-echo "I am root" && id
-
 jq '.username = env.FACTORIO_USERNAME | .password = env.FACTORIO_PASSWORD | .name = env.FACTORIO_SERVER_NAME | .description = env.FACTORIO_SERVER_DESCRIPTION' server-settings-template.json > server-settings.json
 
-cat server-settings.json
-
-whoami 
 nohup /opt/factorio/bin/x64/factorio --start-server-load-latest --server-settings server-settings.json 
-
-bash -c "while true; do sleep 1; done"
