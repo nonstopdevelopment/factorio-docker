59dc3022d1524947cb6ba7ea98b2a9feb60e4bf4
nonstopdevelopment <bryan+git@nonstopdev.com>
2025-04-17 23:05:51 -0400

    add whoami

--- entry_point.sh
+++ entry_point.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cd /factorio/factorio
 
+whoami
+
 jq '.username = env.FACTORIO_USERNAME | .password = env.FACTORIO_PASSWORD | .name = env.FACTORIO_SERVER_NAME | .description = env.FACTORIO_SERVER_DESCRIPTION' server-settings-template.json > server-settings.json
 
 nohup /factorio/factorio/bin/x64/factorio --start-server-load-latest --server-settings server-settings.json 
